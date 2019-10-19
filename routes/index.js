6baede48c475ed7f4d91c891f326320446dc684c
Martha Sharpe <marthasharpe2020@gmail.com>
2019-10-19 12:55:08 -0400

    Install Express validator and session

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Form Validation', success: false, errors: req.session.errors });
+  req.session.errors = null;
 });
 
-router.get('/test/:id', function(req, res, next) {
-  res.render('test', { output: req.params.id });
+router.get('/submit', function(req, res, next) {
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Invalid password').isLength({min: 4}).equals(req.body.confirmPassword);
+  
+  var errors = req.validationErrors();
+  if(errors) {
+    req.session.errors = errors;
+  }
+  res.redirect('/');
 })
-router.post('/test/submit', function(req, res, next) {
-  var id = req.body.id
-  res.redirect('/test/' + id);
-})
+
+// router.get('/test/:id', function(req, res, next) {
+//   res.render('test', { output: req.params.id });
+// })
+// router.post('/test/submit', function(req, res, next) {
+//   var id = req.body.id
+//   res.redirect('/test/' + id);
+// })
 
 module.exports = router;
