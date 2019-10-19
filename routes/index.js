f73c7bd9a31f7fc89a22ff0ffedbc7010bcc0671
Martha Sharpe <marthasharpe2020@gmail.com>
2019-10-19 10:48:17 -0400

    Add GET router

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test/:id', function(req, res, next) {
+  res.render('test', { output: req.params.id });
+})
+
 module.exports = router;
